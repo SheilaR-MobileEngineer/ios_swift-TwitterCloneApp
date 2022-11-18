6d7782b5225ddfc61334a25f14babb702b8123fe
Sheila Rodriguez <sheila.rodriguez@dexcom.com>
2022-11-18 13:23:56 -0600

    First Commit

---
+++ TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Sheila Rodriguez on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
