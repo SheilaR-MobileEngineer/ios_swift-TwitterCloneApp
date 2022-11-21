adb4773868ba813fe768685d3c4d9ba0f929946b
Sheila Rodriguez <sheila.rodriguez@dexcom.com>
2022-11-21 12:38:37 -0600

    Tab view

---
+++ Core/Notifications/NotificationsView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationsView.swift
+//  TwitterClone
+//
+//  Created by Sheila Rodriguez on 11/21/22.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text("Notifications View!")
+    }
+}
+
+struct NotificationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationsView()
+    }
+}
